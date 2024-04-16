caceb9bb8c9566316b0ce9b287d3e70082ce20dd
Nikodem <78365377+wiktorowicz3@users.noreply.github.com>
2024-04-16 10:47:34 +0200

    Add files via upload

---
+++ capp/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+
+application = Flask(__name__)
+
+### Code GitHub
+application.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+DBVAR=os.environ.get("DATABASE")
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
+
+### Code Computer
+
+
+db = SQLAlchemy(application)
+bcrypt = Bcrypt(application)
+login_manager= LoginManager(application)
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+
+from capp.home.routes import home
+from capp.methodology.routes import methodology
+from capp.carbon_app.routes import carbon_app
+from capp.users.routes import users
+
+application.register_blueprint(home)
+application.register_blueprint(methodology)
+application.register_blueprint(carbon_app)
+application.register_blueprint(users)
+
+
+
